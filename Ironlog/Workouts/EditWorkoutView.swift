34716d50f7c20568f7755c3b8955d007d278ea02
bstauff <bstauff@users.noreply.github.com>
2022-12-22 07:38:58 -0600

    edit workout view

---
+++ Ironlog/Workouts/EditWorkoutView.swift
@@ -0,0 +1,20 @@
+//
+//  EditWorkoutView.swift
+//  Ironlog
+//
+//  Created by Brian Stauff on 12/22/22.
+//
+
+import SwiftUI
+
+struct EditWorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditWorkoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditWorkoutView()
+    }
+}
