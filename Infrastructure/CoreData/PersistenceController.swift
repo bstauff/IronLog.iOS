66ad3092de28dcd71ab9688b463b7f0de1c30dba
bstauff <bstauff@users.noreply.github.com>
2022-12-25 11:46:25 -0600

    building mock data for previews

--- Infrastructure/CoreData/PersistenceController.swift
+++ Infrastructure/CoreData/PersistenceController.swift
@@ -18,11 +18,51 @@ struct PersistenceController {
         
         let viewContext = controller.container.viewContext
         
-        let workout = Workout(context: viewContext)
+        let squat = Lift(context: viewContext)
+        squat.id = UUID()
+        squat.name = "Squat"
+        squat.trainingMax = 315
         
+        let workout = Workout(context: viewContext)
         workout.id = UUID()
         workout.isComplete = false
         workout.date = Date()
+        
+        let warmUp = WarmupExercise(context: viewContext)
+        warmUp.workout = workout
+        warmUp.isComplete = false
+        warmUp.id = UUID()
+        warmUp.lift = squat
+        
+        let set1 = ExerciseSet(context: viewContext)
+        set1.id = UUID()
+        set1.isComplete = false
+        set1.reps = 5
+        set1.weight = 275
+        set1.exercise = warmUp
+        
+        let set2 = ExerciseSet(context: viewContext)
+        set2.id = UUID()
+        set2.isComplete = false
+        set2.reps = 3
+        set2.weight = 300
+        set1.exercise = warmUp
+        
+        let set3 = ExerciseSet(context: viewContext)
+        set3.id = UUID()
+        set3.isComplete = false
+        set3.reps = 1
+        set3.weight = 315
+        set3.exercise = warmUp
+        
+        var sets = NSOrderedSet(array: [set1, set2, set3])
+        
+        warmUp.exerciseSets = sets
+        
+        
+        warmUp.workout = workout
+        
+        workout.addToWarmupExercises(warmUp)
         
         do {
             try viewContext.save()
