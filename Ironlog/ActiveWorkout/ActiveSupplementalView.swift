58eeb0f3327ef99827e1acdc4adfe89e961de6aa
bstauff <bstauff@users.noreply.github.com>
2023-01-11 07:15:35 -0600

    stubbing out supplemental view

---
+++ Ironlog/ActiveWorkout/ActiveSupplementalView.swift
@@ -0,0 +1,20 @@
+//
+//  ActiveSupplementalView.swift
+//  Ironlog
+//
+//  Created by Brian Stauff on 1/11/23.
+//
+
+import SwiftUI
+
+struct ActiveSupplementalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActiveSupplementalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActiveSupplementalView()
+    }
+}
