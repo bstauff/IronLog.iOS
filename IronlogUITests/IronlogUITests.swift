33cb429ecddc3b09b8c2a10b2aefc494c881250b
bstauff <bstauff@users.noreply.github.com>
2023-02-25 08:13:16 -0600

    setting up for fastlane snapshot

--- IronlogUITests/IronlogUITests.swift
+++ IronlogUITests/IronlogUITests.swift
@@ -51,6 +51,17 @@ class IronlogUITests: XCTestCase {
         XCTAssertEqual("Workouts", text)
     }
     
+    func testSnapshotLiftCatalog() throws {
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
+        app.tabBars["Tab Bar"].buttons["Workouts"].tap()
+        
+        snapshot("0Launch")
+        
+    }
+    
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
