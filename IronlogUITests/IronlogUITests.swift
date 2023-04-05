185827b6642ff20942cfb71bf3963cab1f14fc1c
bstauff <bstauff@users.noreply.github.com>
2023-04-05 07:41:10 -0500

    adding more screenshot tests

--- IronlogUITests/IronlogUITests.swift
+++ IronlogUITests/IronlogUITests.swift
@@ -16,7 +16,7 @@ class IronlogUITests: XCTestCase {
         
         app.tabBars["Tab Bar"].buttons["Lifts"].tap()
         app.navigationBars["Lift Catalog"]/*@START_MENU_TOKEN@*/.buttons["Add"]/*[[".otherElements[\"Add\"].buttons[\"Add\"]",".buttons[\"Add\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        
+        snapshot("02LiftsAddView")
         app.textFields["Lift name"].tap()
         app.textFields["Lift name"].typeText("Squat")
         
@@ -26,5 +26,30 @@ class IronlogUITests: XCTestCase {
         app.buttons["Save"].tap()
         snapshot("01LiftsView")
         app.launch()
+    }
+    
+    func testMainView() throws {
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        app.tabBars["Tab Bar"].buttons["Workouts"].tap()
+        app.navigationBars["Workouts"]/*@START_MENU_TOKEN@*/.buttons["add"]/*[[".otherElements[\"add\"].buttons[\"add\"]",".buttons[\"add\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let liftPicker = app.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Select Lift"]/*[[".cells",".buttons[\"Main Lift, Select Lift\"].staticTexts[\"Select Lift\"]",".staticTexts[\"Select Lift\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        liftPicker.tap()
+        
+        app.collectionViews.cells.element(boundBy: 1).tap()
+        snapshot("03WorkoutsAddView")
+        app.buttons["Create"].tap()
+        snapshot("04WorkoutsView")
+    }
+    
+    func testActiveWorkoutView() throws {
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        app.tabBars["Tab Bar"].buttons["Active Workout"].tap()
+        app.collectionViews.buttons.element(boundBy: 0).tap()
+        app.buttons["BEGIN"].tap()
+        snapshot("05ActiveWorkoutView")
     }
 }
